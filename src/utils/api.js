ea7e3b5bf0b4aca8137c522596f5765cab8f3083
Gent Veseli <gent.veseli1@mail.com>
2021-04-15 22:40:10 +0200

    made http to https

--- src/utils/api.js
+++ src/utils/api.js
@@ -15,7 +15,7 @@ export const axiosInstance = () => {
 // This instanced axios allows for a one time setup of axios when it comes to
 // CSRF tokens and authentiacion with token, and also use of sub domain apps
 const initAxiosInstance = () => {
-    let baseUrl = "http://cloverlabs.io/wp-json/wp/v2";
+    let baseUrl = "https://cloverlabs.io/wp-json/wp/v2";
     const newInstance = axios.create({
         baseURL: baseUrl
     });
